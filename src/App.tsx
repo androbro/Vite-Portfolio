4c91d0c7eeaf2661e84d761c90839ae019120df3
koen de vulder <devulderk@gmail.com>
2024-01-14 13:17:02 +0100

    fix: general layout

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,10 @@ function App() {
 			className="flex min-h-screen flex-col bg-[#121212]"
 		>
 			<Navbar />
-			<div className="container mt-24 mx-auto px-12 py-4">
+			<div
+				style={{ padding: "1em" }}
+				className="container mt-24 mx-auto px-12 py-4"
+			>
 				<HeroSection />
 				<AchievementsSection />
 				<AboutSection />
