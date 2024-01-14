461b3f887a7aad04096fe1e3a2176ec22544de07
koen de vulder <devulderk@gmail.com>
2024-01-14 16:06:28 +0100

    feat: Update email section content and social media links

--- src/components/EmailSection.tsx
+++ src/components/EmailSection.tsx
@@ -51,15 +51,14 @@ export default function EmailSection() {
 				</h5>
 				<p className="text-[#ADB7BE] mb-4 max-w-md">
 					{" "}
-					I&apos;m currently looking for new opportunities, my inbox is always
-					open. Whether you have a question or just want to say hi, I&apos;ll
-					try my best to get back to you!
+					My inbox is always open. Whether you have a question or just want to
+					say hi, I&apos;ll try my best to get back to you!
 				</p>
 				<div className="socials flex flex-row gap-2">
-					<Link to="github.com">
+					<Link to="https://github.com/androbro">
 						<img src={GithubIcon} alt="Github Icon" />
 					</Link>
-					<Link to="linkedin.com">
+					<Link to="https://www.linkedin.com/in/koendevulder/">
 						<img src={LinkedinIcon} alt="Linkedin Icon" />
 					</Link>
 				</div>
