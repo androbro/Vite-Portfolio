a1aeae1818721ec383e02c1f1a716d5ed779d4c5
koen de vulder <devulderk@gmail.com>
2024-01-14 13:39:03 +0100

    fix: Update padding and max width in App component

--- src/App.tsx
+++ src/App.tsx
@@ -10,8 +10,9 @@ import "./App.css";
 function App() {
 	return (
 		<main
-			style={{ overflow: "hidden" }}
-			className="flex min-h-screen flex-col bg-[#121212]"
+			style={{ padding: "1em" }}
+			// style={{ overflow: "auto", padding: "1em" }}
+			className="flex min-h-screen flex-col bg-[#121212] max-w-[100vw]"
 		>
 			<Navbar />
 			<div className="container mt-24 mx-auto px-12 py-4 sm:px-6">
