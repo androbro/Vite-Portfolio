cecadcd57f0dbba3fa28ba3f4ae1788adb8a40b7
koen de vulder <devulderk@gmail.com>
2024-02-17 11:56:09 +0100

    Update download CV link in HeroSection component

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -50,14 +50,15 @@ export default function HeroSection() {
 						>
 							Contact Me
 						</Link>
-						<Link
-							to="/"
-							className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3"
-						>
-							<span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
+						<span className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3">
+							<a
+								href="../../resume.pdf"
+								download
+								className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2"
+							>
 								Download CV
-							</span>
-						</Link>
+							</a>
+						</span>
 					</div>
 				</motion.div>
 				<motion.div
